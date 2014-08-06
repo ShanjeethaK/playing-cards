a10e845dde0b1695d1b54af23bd2e4a849d00b38
Shanjeetha Kirupananthan <skirupananthan@gmail.com>
2014-08-06 13:45:45 -0700

    second change

---
+++ war/war/test_settings.py
@@ -0,0 +1,14 @@
+import os
+
+__author__ = 'shanjee'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "test",
+        "USER": os.environ.get('PG_USER'),
+        "PASSWORD": os.environ.get('PG_PASSWORD'),
+        "HOST": "127.0.0.1",
+        "PORT": "",
+    }
+}
